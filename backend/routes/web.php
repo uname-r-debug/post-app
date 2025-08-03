857a3f5a0d6a546e1a950eed70103e0d9dae6dbc
uname-r-debug <rahul.nandy@proton.me>
2025-08-03 12:50:52 +0800

    Refactored code

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,53 +4,18 @@ use App\Http\Controllers\UserModelController;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-[$userController, $middleware] = [UserModelController::class, VerifyCsrfToken::class];
-
-Route::post(
-    uri: '/api/create',
-    action: [$userController, 'create']
-)
-    ->withoutMiddleware(
-        $middleware
-    );
-
-Route::post(
-    uri: '/api/delete',
-    action: [$userController, 'delete']
-)
-    ->withoutMiddleware(
-        $middleware
-    );
-
-Route::post(
-    uri: '/api/read',
-    action: [$userController, 'read']
-)
-    ->withoutMiddleware(
-        $middleware
-    );
-
-Route::post(
-    uri: '/api/index',
-    action: [$userController, 'index']
-)
-    ->withoutMiddleware(
-        $middleware
-    );
-
-Route::post(
-    uri: '/api/create.post',
-    action: [$userController, 'createPost']
-)
-    ->withoutMiddleware(
-        $middleware
-    );
-
-Route::post(
-    uri: '/api/delete.post',
-    action:
-    [$userController, 'deletePost']
-)
-    ->withoutMiddleware(
-        $middleware
-    );
+Route::withoutMiddleware(VerifyCsrfToken::class)
+    ->group(function (Illuminate\Routing\Router $router) {
+        foreach ([
+            'create' => 'create',
+            'create.post' => 'createPost',
+            'delete' => 'delete',
+            'delete.post' => 'deletePost',
+            'read' => 'read',
+            'index' => 'index'
+        ] as $route => $controller)
+            $router->post(
+                uri: "/api/$route",
+                action: [UserModelController::class, $controller]
+            );
+    });
